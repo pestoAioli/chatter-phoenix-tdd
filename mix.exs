862af9ea6cdc55235320384bdfc66f3a5bfc1f40
Ricky <drickyriverab@gmail.com>
2022-07-04 15:12:43 -0700

    first red, green cycle done :-)

--- mix.exs
+++ mix.exs
@@ -36,6 +36,7 @@ defmodule Chatter.MixProject do
       {:phoenix, "~> 1.6.6"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
+      {:ex_machina, "~> 2.7.0", only: :test},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
